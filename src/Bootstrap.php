286f14216164125392e767768066407adddd0c4a
Tigrov <rrr-r@ya.ru>
2019-07-19 17:36:34 +0300

    update for migrations

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -19,6 +19,11 @@ class Bootstrap implements \yii\base\BootstrapInterface
     public function bootstrap($app)
     {
         if ($app instanceof \yii\console\Application) {
+            if (!isset($app->controllerMap['migrate'])) {
+                $app->controllerMap['migrate']['class'] = 'yii\console\controllers\MigrationController';
+            } elseif (is_string($app->controllerMap['migrate'])) {
+                $app->controllerMap['migrate']['class'] = $app->controllerMap['migrate'];
+            }
             $app->controllerMap['migrate']['migrationNamespaces'][] = 'tigrov\country\migrations';
         }
     }
